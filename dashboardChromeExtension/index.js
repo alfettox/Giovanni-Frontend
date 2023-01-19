0dac6befa10af3f71a6dc2e2d6bb126b2a1e742c
Giovanni De Franceschi <alfetto@libero.it>
2023-01-18 19:34:50 -0500

    Update 18 jan 2023

--- dashboardChromeExtension/index.js
+++ dashboardChromeExtension/index.js
@@ -55,9 +55,7 @@ fetch("https://api.coingecko.com/api/v3/coins/ethereum")
 
 function getCurrentTime() {
     const date = new Date()
-    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
-    document.getElementById("time-it").textContent = Number.parseInt(date.toLocaleTimeString("en-us", {timeStyle: "short"}))+6 + date.toLocaleTimeString("en-us", {timeStyle: "short"});
-}
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})}
 
 setInterval(getCurrentTime, 1000)
 
