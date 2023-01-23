33875b0b3f095e4b9e53bc84acf1d6a59f88f40f
Giovanni De Franceschi <alfetto@libero.it>
2023-01-23 09:41:55 -0500

    animation update

--- boredBOTAPI/index.js
+++ boredBOTAPI/index.js
@@ -1,26 +1,11 @@
-/**
-Challenge: 
-
-- Start building out the BoredBot Skeleton however you'd like. 
-    That will include:
-    - A title for the app ("BoredBot" might be a good start 😉)
-    - A placeholder element that will be populated with the random 
-      idea we get from the API // id idea
-    - A button to click for triggering the GET request to the Bored API. 
-      (Don't worry about implementing the button quite yet)  //action-button
-*/
 const hereDisplay = document.getElementById("idea");
+const url ="https://apis.scrimba.com/bored/api/activity";
+const btn = document.getElementById("action-button");
 
-const url ="https://apis.scrimba.com/bored/api/activity"
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data)
-//         document.getElementById("activity-name").textContent = data.activity
-//     })
 function fetchFn(){
 fetch(url).then(response => response.json()).then(data => {
     console.log(data);
-    hereDisplay.innerHTML= `ACTIVITY: ${data.activity}, \n
+    hereDisplay.textContent = `ACTIVITY: ${data.activity},\n
                             TYPE: ${data.type},\n
                             PARTICIPANTS:${data.participants},\n
                             PRICE:${data.price},\n
@@ -29,6 +14,5 @@ fetch(url).then(response => response.json()).then(data => {
 })
 }
 
-const btn = document.getElementById("action-button")
+btn.addEventListener('click', fetchFn);
 
-btn.addEventListener('click', fetchFn)
